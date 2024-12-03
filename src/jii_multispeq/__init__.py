c61c803cae8ae592c2ec0a1360fcf44c743d7734
Sebastian <sebastian.kuhlgert@gmail.com>
2024-12-03 18:12:33 +0100

    include new modules

--- src/jii_multispeq/__init__.py
+++ src/jii_multispeq/__init__.py
@@ -15,5 +15,8 @@ __all__ = ['__version__']
 
 from jii_multispeq.about import version
 
+import jii_multispeq.analysis
+import jii_multispeq.device
+import jii_multispeq.measurement
 import jii_multispeq.project
 import jii_multispeq.protocol

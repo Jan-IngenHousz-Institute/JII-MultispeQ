43bed8ba9e7aaa1a6533385b64f91e5b6f8ecc8e
Sebastian <sebastian.kuhlgert@gmail.com>
2024-12-19 01:15:12 +0100

    add get_memory

--- src/jii_multispeq/device/__init__.py
+++ src/jii_multispeq/device/__init__.py
@@ -3,5 +3,5 @@ Serial communication with a MultispeQ device to take measurements
 using protocols and send commands.
 """
 
-from jii_multispeq.device.command import send_command, is_connected, CMDS
+from jii_multispeq.device.command import send_command, is_connected, get_memory, CMDS
 from jii_multispeq.device.connection import connect, disconnect, get_ports

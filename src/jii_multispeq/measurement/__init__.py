f5af5b56e77dd06c1d005db71993c864ddf8058b
Sebastian <sebastian.kuhlgert@gmail.com>
2025-06-27 15:15:43 +0200

    expose to_df function

--- src/jii_multispeq/measurement/__init__.py
+++ src/jii_multispeq/measurement/__init__.py
@@ -7,7 +7,7 @@ as well as from notebooks saved by the PhotosynQ Desktop Application
 (limited support).
 """
 
-from jii_multispeq.measurement.measure import measure, analyze, view
+from jii_multispeq.measurement.measure import measure, analyze, view, to_df
 from jii_multispeq.measurement.checksum import get_crc32, strip_crc32
 from jii_multispeq.measurement.file import list_files, load_files, load_files_df
 from jii_multispeq.measurement.notebook import import_notebook

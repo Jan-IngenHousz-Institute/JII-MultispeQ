d294a8d0869c833c6b14344234b40e6ff18babeb
Sebastian <sebastian.kuhlgert@gmail.com>
2025-06-27 13:44:18 +0200

    Update Regex and add one for cli

--- src/jii_multispeq/constants.py
+++ src/jii_multispeq/constants.py
@@ -3,11 +3,12 @@ Constants
 """
 
 ## Test for the end of MultispeQ output
-REGEX_RETURN_END =      r'([\}][\r|\n]{0,})|(\]\]\}[\r\n]+)|(\}[0-9a-f]{8}\s*$)'
+REGEX_RETURN_END =      r'(([\]]{2}[\}][\r|\n]{1,})|([\}][0-9a-f]{8}[\s\r|\n]{0,})$)'
+REGEX_RETURN_END_CLI =  r'(bad command:[\d\s\w]+)|(\})|(.*?Ready)$'
 
 ## Detect nil and NaN values
-REGEX_NIL_NAN =         r'[\"\s]*(nil|-?NAN)[\"\s]*'
+REGEX_NIL_NAN =         r'\s*"?(nil|-?NAN)"?\s*'
 
 ## Test for checksum at the data string
 REGEX_CHECKSUM =        r'\}[0-9a-f]{8}$'
-REGEX_CHECKSUM_RNS =    r'\}[0-9a-f]{8}[\s\r|\n]?$'
+REGEX_CHECKSUM_RNS =    r'\}[0-9a-f]{8}\s?$'
